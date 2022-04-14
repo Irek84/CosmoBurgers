ccac477528e9041aea2bebc29569b56de9c0473f
i.nafikov <i.nafikov@modumtrans.ru>
2022-04-14 17:58:13 +0300

    AppHeader begins

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import appHeaderStyles from './app-header.module.css';
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components'
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <header className={appHeaderStyles.header}>
+        ffffffff
+      </header>
+    );
+  }
+}
+
+export default AppHeader;
